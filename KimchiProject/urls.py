13acb68df304cea9a618765c552da487f3b1c584
suchan5 <suchan5@hotmail.com>
2020-08-07 16:08:59 +0000

    Added Reviews app

--- KimchiProject/urls.py
+++ KimchiProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import kimchis.views
+import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', kimchis.views.index)
+    path('home/', kimchis.views.index),
+    path('reviews/', reviews.views.index)
 ]
