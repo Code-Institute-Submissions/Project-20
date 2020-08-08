e470e49e287ba9b89131bbd7451359502bae5d74
suchan5 <suchan5@hotmail.com>
2020-08-08 11:00:05 +0000

    Make migrations and migrate for 'Kimchi' class

--- kimchis/models.py
+++ kimchis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Kimchi(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+    desc = models.TextField(blank=False)
+    price = models.FloatField(blank=False)
+    manufactured_date = models.DateField(blank=False)
+    expiry_date = models.DateField(blank=False)
+
+    def __str__(self):
+        return self.title + " " + self.desc
+
