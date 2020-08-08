c74a3544820828a6345e7de58be31b40dc4f4346
suchan5 <suchan5@hotmail.com>
2020-08-08 15:23:22 +0000

    Added one to many relationships between Kimchi and MainIngredient

--- kimchis/admin.py
+++ kimchis/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Kimchi
+from .models import Kimchi, MainIngredient
 
 # Register your models here.
 admin.site.register(Kimchi)
+admin.site.register(MainIngredient)
--- kimchis/models.py
+++ kimchis/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
 
-# Create your models here.
+class MainIngredient(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
 class Kimchi(models.Model):
     title = models.CharField(blank=False, max_length=255)
     desc = models.TextField(blank=False)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     manufactured_date = models.DateField(blank=False)
     expiry_date = models.DateField(blank=False)
+    main_ingredient = models.ForeignKey(
+        MainIngredient, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
