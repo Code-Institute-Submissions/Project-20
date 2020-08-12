659c9d7ca791a9ae356cec70ba727151b527c2ec
suchan5 <suchan5@hotmail.com>
2020-08-12 13:55:10 +0000

    Added placeholder and change to label name in the search engine

--- kimchis/forms.py
+++ kimchis/forms.py
@@ -3,6 +3,9 @@ from .models import MainIngredient
 
 
 class SearchForm(forms.Form):
-    title = forms.CharField(max_length=100, required=False)
+    title = forms.CharField(max_length=100, required=False,
+                            label="Product name",
+                            widget=forms.TextInput(
+                                attrs={'placeholder': 'Search'}))
     main_ingredient = forms.ModelChoiceField(
         queryset=MainIngredient.objects.all(), required=False)
