b2c91c510eb11fd7777ef435c0712ef02dd4520f
suchan5 <suchan5@hotmail.com>
2020-08-14 15:28:04 +0000

    Uploading images in create review at userside

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import Review, Comment
+from cloudinary.forms import CloudinaryJsFileField
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ('title', 'product_name', 'content')
+        fields = ('title', 'product_name', 'content', 'cover')
+    cover = CloudinaryJsFileField()
 
 
 class CommentForm(forms.ModelForm):
