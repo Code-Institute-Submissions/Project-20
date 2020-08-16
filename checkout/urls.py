827ddbd26d89f8f771584ee6dfa88a5be0302395
suchan5 <suchan5@hotmail.com>
2020-08-16 13:34:40 +0000

    Added checkout route

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,7 +4,7 @@ import checkout.views
 
 
 urlpatterns = [
-    path('', checkout.views.checkout),
+    path('', checkout.views.checkout, name="checkout_route"),
     path('success', checkout.views.checkout_success),
     path('cancelled', checkout.views.checkout_cancelled)
 ]
