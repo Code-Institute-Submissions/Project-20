4b929682f1c4574fcf6ddd6f80359f348e0fc720
suchan5 <suchan5@hotmail.com>
2020-08-17 15:12:40 +0000

    Added sending email via gmail

--- KimchiProject/settings.py
+++ KimchiProject/settings.py
@@ -106,7 +106,19 @@ LOGIN_REDIRECT_URL = '/home'
 
 WSGI_APPLICATION = 'KimchiProject.wsgi.application'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Simulate Django to send out email
+TEST_EMAIL = os.environ.get('TEST_EMAIL')
+
+if TES_EMAIL == 1:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
