961d9be41690e3fd9383f57952349dd06485c883
suchan5 <suchan5@hotmail.com>
2020-08-26 17:35:14 +0000

    Amended placeholder in search bar for more defined example

--- kimchis/forms.py
+++ kimchis/forms.py
@@ -6,6 +6,6 @@ class SearchForm(forms.Form):
     title = forms.CharField(max_length=100, required=False,
                             label="Product name",
                             widget=forms.TextInput(
-                                attrs={'placeholder': 'E.g. Radish Kimchi'}))
+                                attrs={'placeholder': 'E.g. Leek Kimchi'}))
     main_ingredient = forms.ModelChoiceField(
         queryset=MainIngredient.objects.all(), required=False)
